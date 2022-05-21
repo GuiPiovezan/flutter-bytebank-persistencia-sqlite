7cf980e981cc6357b7a85faef8e03fc89aafe41e
GuiPiovezan <guilherme.piovezan00@gmail.com>
2022-05-20 22:08:36 -0300

    Criando função para salvar um contato e listar todos os contatos existentes na base de dados

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,14 +1,44 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-void creteDatabase() {
-  getDatabasesPath().then((value) {
+import '../models/contact.dart';
+
+Future<Database> creteDatabase() {
+  return getDatabasesPath().then((value) {
     final String path = join(value, 'bytebank.db');
-    openDatabase(path, onCreate: (db, version) {
-      db.execute('CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY, '
-          'name TEXT, '
-          'account_number INTEGER');
-    }, version: 1);
+    return openDatabase(path, onCreate: (db, version) {
+      db.execute('''CREATE TABLE contacts(
+          id INTEGER PRIMARY KEY, 
+          name TEXT, 
+          account_number INTEGER)''');
+    }, version: 2);
+  });
+}
+
+Future<int> save(Contact contact) {
+  return creteDatabase().then((db) {
+    final Map<String, dynamic> contactMap = {};
+
+    contactMap['name'] = contact.name;
+    contactMap['account_number'] = contact.accountNumber;
+
+    return db.insert('contacts', contactMap);
+  });
+}
+
+Future<List<Contact>> findAll() {
+  return creteDatabase().then((value) {
+    return value.query('contacts').then((maps) {
+      final List<Contact> contacts = [];
+      for (Map<String, dynamic> map in maps) {
+        final Contact contact = Contact(
+          map['id'],
+          map['name'],
+          map['account_number'],
+        );
+        contacts.add(contact);
+      }
+      return contacts;
+    });
   });
 }
